cb95f747705d0834c746a423161c1976a57b089b
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-02 11:54:08 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,9 @@
+import calendar
+print(calendar.weekheader(3)) #prints the week names
+print()  #adding a new line
+print(calendar.firstweekday())  #printing the index of monday
+print()
+
+print(calendar.month(2019,3))  #printing the 3rd month of 2019
+print()
+print(calendar.monthcalendar(2020,11))  #printing the month in matrix form
