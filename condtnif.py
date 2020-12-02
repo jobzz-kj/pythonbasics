326275fba2b6495758f2baff0282ad15b2ec77ba
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-03 01:22:31 +0530

    Update condtnif.py

--- condtnif.py
+++ condtnif.py
@@ -10,3 +10,10 @@ def square():
   jobzz_turtle.forward(100)
   jobzz_turtle.right(90)
   jobzz_turtle.forward(100)
+  
+#if elephant w8 is greater than ant's w8 draw a square
+elephant_w8 = 3000
+ant_w8 = 1
+if elephant_w8 > ant_w8:
+  square()
+
