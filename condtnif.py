b4c34969a87dce32967f4fd56ec73a286db0585e
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-02 21:30:42 +0530

    Create condtnif.py

---
+++ condtnif.py
@@ -0,0 +1,12 @@
+import turtle
+jobzz_turtle=turtle.Turtle()
+
+def square():
+  
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
