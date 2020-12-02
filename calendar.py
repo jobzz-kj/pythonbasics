845a734e535176df7874256e58130968d808c074
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-02 11:56:10 +0530

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -7,3 +7,4 @@ print()
 print(calendar.month(2019,3))  #printing the 3rd month of 2019
 print()
 print(calendar.monthcalendar(2020,11))  #printing the month in matrix form
+print(calendar.calendar(2020))
