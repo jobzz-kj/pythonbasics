b41d7e0604ed995b40ea6fb6edca0b94cb9d8b0a
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-04 01:32:05 +0530

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,11 @@
+#casting--changing one data type to another data type
+#If you want to call a function that excepts a certain data type to be given to it, but you have a different data 
+#type, you can cast the data to the right data type before you give it to the function
+x=1
+float(x) #changes int to float
+print(x)
+
+str(x) #changes x to string
+
+##anything that is not zero is considered to be TRUE!!!! 0--->FALSE
+
