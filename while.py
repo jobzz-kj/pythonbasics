e1d4341a5f7cc498efd2df79a5469ebdc8be0de2
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-04 01:00:29 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,7 @@
+#while loops--basically keep doing stuff over and over again.
+#use- you don't know when something gonna end!! that's when uses while loop
+import turtle
+jobzz_turtle=turtle.Turtle()
+jobzz='happy'
+while jobzz=='happy':  #what is '=='-->it is the equality operator, checks and gives either true or false
+  jobzz_turtle.forward(10) #which results in an infinite loop....
