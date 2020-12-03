809139af7abec160dbb92dcc00352ab199b4792d
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2020-12-04 01:38:01 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+#string data type
+#things that are enclosed in a quotation mark!!
+#just a sentence,can have one one char..
+#we can do things like:
+#-convert strings to uppercase or lowercase
+#-get or replace parts
+#-split a string into parts
+#-construct strings from other data types and it goes on....
+
+s="hello"
+print(s)
+print(s[1])
